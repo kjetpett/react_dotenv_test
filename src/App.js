7319315e9558b948b261c4e2be90bcc31e78c506
Kjetil Pettersson <kjetil.pttersson@gmail.com>
2021-04-29 22:44:07 +0200

    env oppsett

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br/>
+          Environment: {process.env.NODE_ENV}<br/>
+          Testvar: {process.env.REACT_APP_Testvar}
         </p>
         <a
           className="App-link"
